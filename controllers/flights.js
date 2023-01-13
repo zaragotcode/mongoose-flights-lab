9e69292b2d26a7b30664a53aa0dfe58147bece53
Patrick Mazara <patrickmazara@hotmail.com>
2023-01-13 08:30:24 -0500

    add create and new function

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,14 +1,44 @@
 import { Flight } from "../models/flight.js"
 
+
+
+function newFlight(req, res) {
+  res.render("flight/new", {
+    title: "Add Flight"
+  })
+}
+
+
+function create(req, res) {
+  Flight.create(req.body)
+  .then(flight => {
+    res.redirect(`/flight/${flight._id}`)
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/flights')
+  })
+}
+
+
 function index(req, res) {
   Flight.find({})
   .then(flights => {
     res.render('flights/index')
-      title: 'All Flights'
       flights
+  })
+  .catch(error => {
+    // handle errors
+    console.log(error)
+    res.redirect('/flights')
   })
 }
 
+
+
+
 export {
-  index
+  index,
+  newFlight as new,
+  create
 }
--- routes/flights.js
+++ routes/flights.js
@@ -2,8 +2,12 @@ import { Router } from 'express'
 import * as flightCtrl from "../controllers/flights.js"
 const router = Router()
 
-/* GET users listing. */
+// Get localhost:3000/flights
 router.get('/', flightCtrl.index)
+
+// Get localhost:3000/flights/new
+router.get('/new', flightCtrl.new)
+
 
 export {
   router
